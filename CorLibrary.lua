f279977ad6fecd500eaed7dd49b92cc9fba3498d
Cortex <151866914+anilenzo@users.noreply.github.com>
2024-07-03 19:55:04 +0300

    CorLibrary.lua

--- CorLibrary.lua
+++ CorLibrary.lua
@@ -1,1 +1,7 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/anilenzo/Corlib/main/Windw.lua")))()
 
+local Tab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
