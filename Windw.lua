cd3e93d3cc0b708432559950dd8cac91cb0196ca
Cortex <151866914+anilenzo@users.noreply.github.com>
2024-07-03 22:02:49 +0300

    Delete Windw.lua

--- Windw.lua
+++
@@ -1,4 +0,0 @@
-local CorLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/anilenzo/Corlib/main/Corlib.lua")))()
-
-local Window = CorLib:MakeWindow({Name = "Main", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
---Owner: Cortex / Changes from OrionLib
