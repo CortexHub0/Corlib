c3c18da1bc8a9f30fa69c6d149fad2a6c6240006
Cortex <151866914+anilenzo@users.noreply.github.com>
2024-07-03 19:50:30 +0300

    Windw.lua

---
+++ Windw.lua
@@ -0,0 +1,4 @@
+local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/anilenzo/Corlib/main/Corlib.lua")))()
+
+local Window = CorLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+--Owner: Cortex / Changes from OrionLib
