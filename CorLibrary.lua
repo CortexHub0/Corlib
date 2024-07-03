e055750896474e5b7ce4e4bf413e44abc615ec7e
Cortex <151866914+anilenzo@users.noreply.github.com>
2024-07-03 22:02:34 +0300

    CorLibrary.lua

--- CorLibrary.lua
+++ CorLibrary.lua
@@ -1,7 +1,14 @@
-local CorLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/anilenzo/Corlib/main/Windw.lua")))()
+local CorLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/anilenzo/Corlib/main/Windw.lua"))()
+
+local Window = CorLib:MakeWindow({
+    Name = "Main",
+    HidePremium = false,
+    SaveConfig = true,
+    ConfigFolder = "OrionTest"
+})
 
 local Tab = Window:MakeTab({
-	Name = "Main",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+    Name = "Main",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
