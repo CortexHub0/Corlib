7d0586f27400491e3440b7b561853216aecda330
Cortex <151866914+anilenzo@users.noreply.github.com>
2024-07-17 17:14:57 +0300

    CorLibrary.lua

--- CorLibrary.lua
+++ CorLibrary.lua
@@ -1,14 +1,11 @@
 local CorLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/anilenzo/Corlib/main/Corlib.lua"))()
 
-local Window = CorLib:MakeWindow({
-    Name = "Main",
-    HidePremium = false,
-    SaveConfig = true,
-    ConfigFolder = "OrionTest"
-})
-
-local Tab = Window:MakeTab({
-    Name = "Main",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
+local Window = Rayfield:CreateWindow({
+   Name = " ",
+   LoadingTitle = "CorLib",
+   LoadingSubtitle = "CoLib",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   }
